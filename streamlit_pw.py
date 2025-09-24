d9cbee9b3c71425af2d6b03f7c7e9185624dde11
GitService4Com <radanmes@4comtecnologia.com.br>
2025-09-24 13:23:54 -0300

    Add files via upload

--- streamlit_pw.py
+++ streamlit_pw.py
@@ -7,30 +7,18 @@ power_bi_links = [
 		"https://app.powerbi.com/view?r=eyJrIjoiMGVjYzVjZjktMjAxYy00NzkxLTkxMDUtMmM1Y2VlZGQ4OTc1IiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9"
 ]
 
-# Tempo em segundos para trocar de aba
-switch_interval = 30
+# Tempo entre trocas de aba (em segundos)
+refresh_time = 30
 
-# Armazena o tempo da última troca
-if 'last_switch' not in st.session_state:
-    st.session_state.last_switch = time.time()
+# Posição atual (salva no estado da sessão)
+if "page_index" not in st.session_state:
     st.session_state.page_index = 0
-
-# Verifica se passou o tempo de troca
-if time.time() - st.session_state.last_switch > switch_interval:
+else:
     st.session_state.page_index = (st.session_state.page_index + 1) % len(power_bi_links)
-    st.session_state.last_switch = time.time()
 
-# Mostra o iframe com o relatório atual
+# Mostra o iframe do Power BI
 st.components.v1.iframe(power_bi_links[st.session_state.page_index], height=600)
 
-# Adiciona auto-refresh usando JavaScript
-st.markdown(
-    f"""
-    <script>
-        setTimeout(function(){{
-            window.location.reload();
-        }}, {switch_interval * 1000});
-    </script>
-    """,
-    unsafe_allow_html=True
-)
+# Aguarda e força recarregamento da página Streamlit
+time.sleep(refresh_time)
+st.experimental_rerun()

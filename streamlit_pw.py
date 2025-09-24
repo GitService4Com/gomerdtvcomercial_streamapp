efdf2a6e94bfe3ad667144a43f968f096409d1c9
GitService4Com <radanmes@4comtecnologia.com.br>
2025-09-24 12:54:39 -0300

    Add files via upload

---
+++ streamlit_pw.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import time
+
+# Lista de URLs públicas do Power BI (uma para cada aba simulada)
+power_bi_links = [
+        "https://app.powerbi.com/view?r=eyJrIjoiMTFkOGU0YjYtZTdmMy00NmNmLTk0YmYtNDQ3MGYyMzVkZmUwIiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9&pageName=f624eba97bb38c6282d6",
+		"https://app.powerbi.com/view?r=eyJrIjoiMTFkOGU0YjYtZTdmMy00NmNmLTk0YmYtNDQ3MGYyMzVkZmUwIiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9&pageName=8e8e380329d0d0eed971",
+		"https://app.powerbi.com/view?r=eyJrIjoiMTFkOGU0YjYtZTdmMy00NmNmLTk0YmYtNDQ3MGYyMzVkZmUwIiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9&pageName=0b8cc8b66f54517864d",
+		"https://app.powerbi.com/view?r=eyJrIjoiMTFkOGU0YjYtZTdmMy00NmNmLTk0YmYtNDQ3MGYyMzVkZmUwIiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9&pageName=4c447571bff13334cf33",
+		"https://app.powerbi.com/view?r=eyJrIjoiMTFkOGU0YjYtZTdmMy00NmNmLTk0YmYtNDQ3MGYyMzVkZmUwIiwidCI6Ijk0MTc0NTRiLTRhYWMtNDY5MS04MWIyLTc3NmU3OWI5MzA0YiJ9&pageName=6fde331acb98af207c25"
+]
+
+# Tempo entre trocas de aba (em segundos)
+refresh_time = 30
+
+# Posição atual (salva no estado da sessão)
+if "page_index" not in st.session_state:
+    st.session_state.page_index = 0
+else:
+    st.session_state.page_index = (st.session_state.page_index + 1) % len(power_bi_links)
+
+# Mostra o iframe do Power BI
+st.components.v1.iframe(power_bi_links[st.session_state.page_index], height=600)
+
+# Aguarda e força recarregamento da página Streamlit
+time.sleep(refresh_time)
+st.experimental_rerun()
